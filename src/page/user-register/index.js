8e3c87cc0a5f7cae65ad7395e7243b51220e02b9
FangFeiyue <18766131540@163.com>
2017-08-15 17:07:42 +0800

    验证注册信息

--- src/page/user-register/index.js
+++ src/page/user-register/index.js
@@ -30,6 +30,10 @@ var page = {
         // 验证username
         $('#username').blur(function(){
             var username = $.trim($(this).val());
+            // 如果用户名为空，不做验证
+            if (!username){
+                return '';
+            }
             // 异步验证用户名是否存在
             _user.checkUsername(username, function(res){
                 formError.hidde();               
@@ -54,15 +58,20 @@ var page = {
         var formData = {
              username: $.trim($('#username').val()),    
              password: $.trim($('#password').val()),    
-        },
+             passwordConfirm : $.trim ($('#password-confirm').val()),
+             phone: $.trim($('#phone').val()),      
+             email: $.trim($('#email').val()),    
+             question : $.trim($('#question').val()),      
+             answer: $.trim($('#answer').val())    
+        }, 
         // 表单验证结果
         validateResult = this.formValidate(formData);
 
         // 验证成功
         if (validateResult.status){
             //  提交
-            _user.login(formData, function(res){
-                window.location.href = tool.getUrlParam('redirect') || './index.html';
+            _user.register(formData, function(res){
+                window.location.href = './result.html?type=register';
                  
             }, function(errMsg){
                 formError.show(errMsg);
@@ -86,6 +95,41 @@ var page = {
 
          if (!tool.validate(formData.password, 'require')){
             result.msg = '密码不能为空'; 
+            return result;
+        }
+
+        if (formData.password.length < 6){
+            result.msg = '密码长度不能少于6位';
+            return result;
+        }
+
+        // 两次输入的密码是否一致
+        if (formData.password !== formData.passwordConfirm){
+            result.msg = '两次输入的密码不一致';
+            return result;
+        }
+
+        // 验证手机号
+        if (!tool.validate(formData.phone, 'phone')){
+            result.msg = '手机格式不正确'; 
+            return result;
+        }
+
+        // 验证邮箱
+        if (!tool.validate(formData.email, 'email')){
+            result.msg = '邮箱格式不正确'; 
+            return result;
+        }
+
+        // 验证密码提示问题
+        if (!tool.validate(formData.question, 'question')){
+            result.msg = '密码提示问题不能为空'; 
+            return result;
+        }
+
+        // 验证密码答案 
+        if (!tool.validate(formData.answer, 'answer')){
+            result.msg = '密码提示问题的答案不能为空'; 
             return result;
         }
 
