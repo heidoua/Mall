9556d7ad6da3d2db99c5fa5505aabdfd7e363bb7
FangFeiyue <18766131540@163.com>
2017-08-16 18:13:35 +0800

    配置个人中心

--- webpack.config.js
+++ webpack.config.js
@@ -42,6 +42,8 @@ var config = {
         'common': ['./src/page/common/index.js'],
         'index': ['./src/page/index/index.js'],
         'user-login': ['./src/page/user-login/index.js'],
+        'user-center': ['./src/page/user-center/index.js'],
+        'user-center-update': ['./src/page/user-center-update/index.js'],
         'user-register': ['./src/page/user-register/index.js'],
         'user-pass-reset': ['./src/page/user-pass-reset/index.js'],
         'result': ['./src/page/result/index.js']
@@ -90,7 +92,9 @@ var config = {
         new HtmlWebpackPlugin(getHtmlConfig('user-login', '用户登录')),
         new HtmlWebpackPlugin(getHtmlConfig('result', '操作结果')),
         new HtmlWebpackPlugin(getHtmlConfig('user-register', '用户注册')),
-        new HtmlWebpackPlugin(getHtmlConfig('user-pass-reset', '找回密码'))
+        new HtmlWebpackPlugin(getHtmlConfig('user-pass-reset', '找回密码')),
+        new HtmlWebpackPlugin(getHtmlConfig('user-center', '个人中心')),
+        new HtmlWebpackPlugin(getHtmlConfig('user-center-update', '修改个人信息'))
     ]
 };
 
