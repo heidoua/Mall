d0ca0b2851af46fb2929e0b5018ad6cb830505db
FangFeiyue <18766131540@163.com>
2017-08-16 16:22:26 +0800

    找回密码逻辑部分

--- src/page/user-pass-reset/index.js
+++ src/page/user-pass-reset/index.js
@@ -22,68 +22,100 @@ var formError = {
 
 //page逻辑部分
 var page = {
+    data: {
+        username: '',
+        question: '',
+        answer: '',
+        token : ''
+    },
     init:  function(){
+        this.onLoad();
         this.bindEvent();    
+    },
+    onLoad: function(){
+        this.loadStepUsername();
     },
     bindEvent: function(){
         var _this = this;
-        // 登录按钮的点击
-        $('#submit').click(function(){
-            _this.submit();
-        });    
-
-        // 如果按下回车也进行提交
-        $('.user-content').keyup(function(e){
-            if (e.keyCode == 13){
-                _this.submit();
+        // 输入用户名后下一步按钮的点击
+        $('#submit-username').click(function(){
+            var username = $.trim($('#username').val());
+            // 输入了用户名
+            if (username){
+                _user.getQuestion(username, function(res){
+                    _this.data.username = username; 
+                    _this.data.question = res;
+                    _this.loadStepQuestion(); 
+                }, function(errMsg){
+                    formError.show(errMsg);
+                }); 
+            }else{// 没有输入用户名
+                formError.show('请输入用户名');
             }
         });
+
+        // 输入密码提示问题答案中的按钮的点击
+        $('#submit-question ').click(function(){
+            var answer  = $.trim($('#anwser').val());
+            // 输入了密码提示问题答案
+            if (answer){
+                // 检查密码提示问题答案
+                _user.checkAnwser({
+                    username: _this.data.username,
+                    question: _this.data.question,
+                    answer: answer
+                }, function(res){
+                    _this.data.anwser = anwser; 
+                    _this.data.token = res;
+                    _this.loadStepPassword(); 
+                }, function(errMsg){
+                    formError.show(errMsg);
+                }); 
+            }else{// 没有输入密码提示问题答案
+                formError.show('请输入密码提示问题的答案');
+            }
+        });     
+
+        // 输入新密码后的按钮点击
+        $('#submit-password ').click(function(){
+            var password  = $.trim($('#password').val());
+            // 输入了新密码
+            console.log(password.length);
+            if (password && password.length >= 6){ 
+                // 检查密码提示问题答案
+                _user.resetPassword({ 
+                    username: _this.data.username,
+                    passwordNew: password,
+                    forgetToken: _this.data.token
+                }, function(res){
+                    window.location.href = './result.html?type=pass-reset'; 
+                }, function(errMsg){
+                    formError.show(errMsg);
+                }); 
+            }else{// 没有输入新密码
+                formError.show('请输入不少于六位的新密码');
+            }
+        });     
     },
-    // 提交表单
-    submit: function(){
-        var formData = {
-             username: $.trim($('#username').val()),    
-             password: $.trim($('#password').val()),    
-        },
-        // 表单验证结果
-        validateResult = this.formValidate(formData);
-
-        // 验证成功
-        if (validateResult.status){
-            //  提交
-            _user.login(formData, function(res){
-                window.location.href = tool.getUrlParam('redirect') || './index.html';
-                 
-            }, function(errMsg){
-                formError.show(errMsg);
-            });
-        }else{// 验证失败
-            // 错误提示
-            formError.show(validateResult.msg);
-        }
+    // 加载输入用户名的一步
+    loadStepUsername: function(){
+        $('.step-username').show();
     },
-    // 表单的验证字段
-    formValidate: function(formData){
-         var result = {
-              status: false,
-              msg: ''
-         };
-
-         if (!tool.validate(formData.username, 'require')){
-             result.msg = '用户名不能为空'; 
-             return result;
-         }
-
-         if (!tool.validate(formData.password, 'require')){
-            result.msg = '密码不能为空'; 
-            return result;
-        }
-
-        //通过验证，返回正确提示
-         result.status = true;
-         result.msg = '验证成功';
-         return result;
-    }
+    // 加载输入密码提示问题答案的一步
+    loadStepQuestion: function(){
+        // 清除错误提示
+        formError.hidde(); 
+        // 做容器的切换
+        $('.step-username').hide().siblings('.step-question').show().find('.question').text(this.data.question);
+    },
+    // 加载输入密码的一步 
+    loadStepPassword: function(){
+        // 清除错误提示
+        formError.hidde(); 
+        // 做容器的切换
+        $('.step-question').hide().siblings('.step-password').show();
+    },
+     
 }; 
 
 $(function(){
