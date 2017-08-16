c17df5c70d21b64e26ab1d92af5fea8ab86bfecd
FangFeiyue <18766131540@163.com>
2017-08-16 18:14:21 +0800

    个人中心页面布局

---
+++ src/page/user-center/index.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: FangFeiyue 
+ * @Date: 2017-08-16 17:55:36 
+ * @Last Modified by: FangFeiyue
+ * @Last Modified time: 2017-08-16 17:58:09
+ */
+
+ require('./index.css');
+ require('page/common/nav/index.js');
+ require('page/common/header/index.js');
+ var navSide = require('page/common/nav-side/index.js');
+ var tool = require('util/tool.js');
