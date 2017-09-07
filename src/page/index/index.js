d07c8de1b80cfe857d0d4abd38c3d9c0eb5fdf56
FangFeiyue <18766131540@163.com>
2017-09-07 14:58:11 +0800

    banner的前进后退

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -55,10 +55,17 @@ navSide.init({
 
 // unslider初始化
 $(function() {
+    // 渲染banner的html
     var  bannerHtml = tool.renderHtml(templateBanner);
     $('.banner-con').html(bannerHtml);
-    $('.banner').unslider({
+    // 初始化banner
+    var $slider = $('.banner').unslider({
         dots: true  
+    }); 
+    // 前一张后一张操作的事件绑定
+    $('.banner-con .banner-arrow').click(function(){
+        var forward  =  $(this).hasClass('prev') ? 'prev' : 'next';
+        $slider.data('unslider')[forward]();
     });
 });
 
