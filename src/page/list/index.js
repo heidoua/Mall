ea3d6813ede69a651128f53d27d4a766a153fe50
FangFeiyue <18766131540@163.com>
2017-09-21 14:53:17 +0800

    加载商品列表

--- src/page/list/index.js
+++ src/page/list/index.js
@@ -2,7 +2,7 @@
  * @Author: FangFeiyue 
  * @Date: 2017-09-08 09:28:11 
  * @Last Modified by: FangFeiyue
- * @Last Modified time: 2017-09-08 09:29:33
+ * @Last Modified time: 2017-09-21 14:30:56
  */
 require("./index.css");
 require("page/common/nav/index.js");
@@ -10,3 +10,51 @@ require("page/common/header/index.js");
 var tool = require("util/tool.js");
 var _product = require("service/product-service.js");
 var templateIndex = require("./index.string");
+
+var page = {
+    data: {
+        listParam: {
+            pageNum: tool.getUrlParam('pageNum') || 1,
+            orderBy: tool.getUrlParam('orderBy') || 'default',
+            keyword: tool.getUrlParam('keyword') || '',
+            pageSize: tool.getUrlParam('pageSize')  || 20,
+            categoryid: tool.getUrlParam('categoryid') || ''
+        }    
+    },
+    init: function(){
+        this.onLoad();
+        this.bindEvent();
+    },
+    onLoad: function(){
+        this.loadList();
+    },
+    bindEvent: function(){
+
+    },
+    // 加载list数据
+    loadList: function(){
+        var _this = this, 
+        listHtml = '', 
+        listParam = this.data.listParam;
+        
+        // 获取list列表
+        _product.getProductList(listParam, function(res){ 
+            listHtml = tool.renderHtml(templateIndex, {
+                list: res.list 
+            });
+            console.log('testtttt====>', listHtml);
+            $('.p-list-con').html(listHtml);
+            _this.loadPagination(res.pageNum,  res.pages);
+        }, function(errMsg){
+            tool.errorTips(errMsg);
+        });     
+    },
+    // 加载分页信息
+    loadPagination: function(pageNum, pages ){
+         
+    }
+};
+
+$(function(){
+    page.init();
+});
--- src/service/product-service.js
+++ src/service/product-service.js
@@ -2,16 +2,16 @@
  * @Author: FangFeiyue 
  * @Date: 2017-09-08 09:30:09 
  * @Last Modified by: FangFeiyue
- * @Last Modified time: 2017-09-08 09:32:17
+ * @Last Modified time: 2017-09-21 14:08:38
  */
 var tool = require('util/tool.js');
 
 var _product = {
     //用户登录
-    login: function(userInfo, resolve, reject){
+    getProductList: function(listParam, resolve, reject){
         tool.request({
-            url: tool.getServerUrl('/user/login.do'),
-            data: userInfo,
+            url: tool.getServerUrl('/product/list.do'),
+            data: listParam,
             method: 'POST',
             success: resolve,
             error: reject
