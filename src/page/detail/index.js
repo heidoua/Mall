4f2b44e841cdcbc9795ee5b5398da43118e6a91d
FangFeiyue <18766131540@163.com>
2017-10-10 14:10:15 +0800

    商品详情页错误提示信息

--- src/page/detail/index.js
+++ src/page/detail/index.js
@@ -4,13 +4,13 @@
  * @Last Modified by: FangFeiyue
  * @Last Modified time: 2017-09-22 15:08:23
  */
-require("./index.css");
+require("./index.c ss");
 var tool = require("util/tool.js");
 require("page/common/nav/index.js");
 require("page/common/header/index.js");
-// var templateIndex = require("./index.string");
+var templateIndex = require("./index.string");
 // var Pagination = require('util/pagination/index.js');
-// var _product = require("service/product-service.js");
+var _product = require("service/product-service.js");
 
 var page = {
     data: {
@@ -31,6 +31,12 @@ var page = {
     },
     // 加载商品详情数据 
     loadDetail: function(){
-
+        var html = '';
+        _product.getProductDetail(this.data.productId, function(res){
+            html = tool.renderHtml(templateIndex, res);
+            $('.page-wrap').html(html);     
+        }, function(errMsg) {
+            $('.page-wrap').html('<p class="err-tip">此商品太淘气，找不到了</p>'); 
+        });
     }
 };
