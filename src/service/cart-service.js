559f16deb6892b2f753c94ac1450876426c4d60d
FangFeiyue <18766131540@163.com>
2017-10-11 10:39:38 +0800

    购物车接口

--- src/service/cart-service.js
+++ src/service/cart-service.js
@@ -2,7 +2,7 @@
  * @Author: FangFeiyue 
  * @Date: 2017-08-15 18:00:53 
  * @Last Modified by: FangFeiyue
- * @Last Modified time: 2017-08-15 18:09:19
+ * @Last Modified time: 2017-10-11 10:02:52
  */
 var tool = require('util/tool.js');
 
@@ -24,7 +24,52 @@ var _cart = {
             error   : reject
         });
     },
-
+    // 获取购物车列表 
+    getCartList : function(resolve, reject){
+        tool.request({
+            url     : tool.getServerUrl('/cart/list.do'),
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 选择购物车商品 
+    selectProduct : function(productId, resolve, reject){
+        tool.request({
+            url     : tool.getServerUrl('/cart/select.do'),
+            data: {
+                productId: productId 
+            }, 
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 取消选择购物车商品 
+    unselectProduct : function(productId, resolve, reject){
+        tool.request({
+            url     : tool.getServerUrl('/cart/un_select.do'),
+            data: {
+                productId: productId 
+            }, 
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 全选 
+    selectAllProduct : function(resolve, reject){
+        tool.request({
+            url     : tool.getServerUrl('/cart/select_all.do'),
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 取消全选 
+    unselectAllProduct : function(resolve, reject){
+        tool.request({
+            url     : tool.getServerUrl('/cart/un_select_all.do'),
+            success : resolve,
+            error   : reject
+        });
+    },
 };
 
 module.exports  = _cart;
