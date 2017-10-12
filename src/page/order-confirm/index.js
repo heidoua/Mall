a67b2611269cee8f6c2af88dfca009fe27b5df42
FangFeiyue <18766131540@163.com>
2017-10-12 19:06:48 +0800

    提交订单

--- src/page/order-confirm/index.js
+++ src/page/order-confirm/index.js
@@ -2,7 +2,7 @@
  * @Author: FangFeiyue 
  * @Date: 2017-10-12 11:18:42 
  * @Last Modified by: FangFeiyue
- * @Last Modified time: 2017-10-12 17:53:09
+ * @Last Modified time: 2017-10-12 18:47:35
  */
 require("./index.css");
 require("page/common/nav/index.js");
@@ -12,7 +12,6 @@ var templateProduct = require("./product-list.string");
 var templateAddress = require("./address-list.string");
 var _order          = require("service/order-service.js"); 
 var _address        = require("service/address-service.js"); 
-
 var page = {
     data: {
         selectedAddressId: null
@@ -25,10 +24,13 @@ var page = {
         // 加载地址列表
         this.loadAddressList();
         // 加载商品列表
-        this.loadProductList();
+        this.loadProductList(); 
     },
     bindEvent: function(){
-        
+        // 选择地址
+        this.selectAddress(this);
+        // 订单提交
+        this.submitOrder(this);
     },
     // 加载地址列表 
     loadAddressList: function(){
@@ -45,9 +47,32 @@ var page = {
         var _this = this;
         _order.getProductList(function(res){
             var productListHtml = tool.renderHtml(templateProduct, res);
-            $('.address-con').html(productListHtml);
+            $('.product-con').html(productListHtml);
         }, function(){
             $('.address-con').html('<p class="err-tip">商品信息加载失败，请刷新后重试</p>');
+        });
+    },
+    // 地址的选择
+    selectAddress: function(_this){
+        $(document).on('click', 'address-item', function(){
+            $(this).addClass('active').siblings('.address-item').removeClass('active');
+            _this.data.selectedAddressId = $(this).data('id');    
+        });
+    },
+    // 订单提交
+    submitOrder: function(_this){
+        $(document).on('click', '.order-submit', function(){
+             var shippingId = _this.data.selectedAddressId;
+             
+             if (!shippingId){
+                _order.createOrder(shippingId, function(res){
+                    window.location.href = './payment.html?orderNumber=' + res.orderNo;
+                }, function(errMsg){
+                    tool.errorTips(errMsg);
+                });
+             }else{
+                 tool.errorTips('请您选择地址后再提交订单');
+             }
         });
     }
 }
--- src/service/order-service.js
+++ src/service/order-service.js
@@ -2,7 +2,7 @@
  * @Author: FangFeiyue 
  * @Date: 2017-10-12 11:20:09 
  * @Last Modified by: FangFeiyue
- * @Last Modified time: 2017-10-12 17:22:07
+ * @Last Modified time: 2017-10-12 18:46:57
  */
 var tool = require('util/tool.js');
  
@@ -14,7 +14,16 @@ var _order = {
             success : resolve,
             error   : reject
         });
-    }
+    } ,
+    // 提交订单
+    createOrder: function(orderInfo, resolve, reject){
+        tool.request({
+            url     : tool.getServerUrl('/order/create.do'),
+            data: orderInfo,
+            success : resolve,
+            error   : reject
+        });
+    },
 };
 
 module.exports  = _order;
